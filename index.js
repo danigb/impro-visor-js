f29e57b01a71d53acc5683a95dd6718c56724a22
danigb <danigb@gmail.com>
2015-04-23 18:45:48 +0200

    First version of the leadsheets

--- index.js
+++ index.js
@@ -4,51 +4,30 @@
 var extname = require('path').extname;
 var Metalsmith = require('metalsmith');
 
-function sheetBuilder(parent) {
-  var part = null;
-  return function(line, object) {
-    console.log("MIERDA", line, object);
-    if(object) {
-      if(object.key == 'style') {
-        parent.styles = parent.styles || {};
-        parent.styles[object.value] = object.props;
-      } else if(object.key == 'part') {
-        part = object.props;
-        parent.parts = parent.parts || [];
-        parent.parts.push(part);
-      } else if(object.key == 'section') {
-        part.section = object.props;
-      } else {
-        parent[object.key] = object.value;
-      }
-    } else {
-      part.literal = part.literal || "";
-      part.literal += line;
-    }
-  }
-}
+var parse = require('./lib/parser');
+var build = require('./lib/builder')
 
-function parse(buffer) {
-  var lines = buffer.split('\n').filter(function(line) { return !/^\s*$/m.test(line); })
-  return lines.map(function() {
 
-  }):
+function processFile(files, file, builder) {
+  var buffer = files[file].contents.toString();
+  var tokens = parse(buffer);
+  var txt = tokens.map(function(t) { return [t.type, t.key, t.value].join('|'); }).join('\n');
+  files[file + ".txt"] = { contents: new Buffer(txt) };
+  var obj = builder(tokens);
+  var json = JSON.stringify(obj, null, 2);
+  files[file + ".json"] = { contents: new Buffer(json) };
+  delete files[file];
 }
 
 
-function leadsheets() {
+function improvisor(){
   return function(files, metalsmith, done) {
     Object.keys(files).forEach(function(file) {
-      if ('.ls' == extname(file)) {
-        var name = file.slice(0, -3);
-        console.log("PROCESSING: " + name);
-        var contents = files[file].contents.toString();
-        //var json = JSON.stringify(obj, null, 2);
-        var json = parse(buffer).join('\n');
-        files[name + ".json"] = {
-          contents: new Buffer(json)
-        }
-        delete files[file];
+      var builder = (extname(file) == '.ls') ? build.leadsheet :
+        (file.indexOf('My.') > 0) ? build.vocab : null;
+
+      if(builder) {
+        processFile(files, file, builder);
       }
     });
     done();
@@ -58,8 +37,7 @@ function leadsheets() {
 console.log("Build.");
 Metalsmith(__dirname)
   .source('source')
-  .source('source/leadsheets/transcriptions/KennyGarrett')
-  .use(leadsheets())
+  .use(improvisor())
   .build(function(err) {
     if (err) throw err;
     console.log("Done!!");
