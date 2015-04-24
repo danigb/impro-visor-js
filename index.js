bc764f70b57a55c2119e4058f8bed022264e0e48
danigb <danigb@gmail.com>
2015-04-24 11:42:55 +0200

    My voc separated in files

--- index.js
+++ index.js
@@ -8,15 +8,30 @@ var parse = require('./lib/parser');
 var build = require('./lib/builder')
 
 
-function processFile(files, file, builder) {
+function serialize(files, file, obj) {
+  var json;
+
+  if (file.indexOf('My.voc') >= 0) {
+    var keys = Object.keys(obj);
+    keys.forEach(function(key) {
+      json = JSON.stringify(obj[key], null, 2);
+      files[file + "." + key + ".json"] = { contents: new Buffer(json) };
+    });
+  } else {
+    json = JSON.stringify(obj, null, 2);
+    files[file + ".json"] = { contents: new Buffer(json) };
+  }
+
+}
+
+function processFile(files, file, builder, serializer) {
   console.log("PROCESS", file);
   var buffer = files[file].contents.toString();
   var tokens = parse(buffer);
 //  var txt = tokens.map(function(t) { return [t.type, t.key, t.value].join('|'); }).join('\n');
 //  files[file + ".txt"] = { contents: new Buffer(txt) };
   var obj = builder(tokens);
-  var json = JSON.stringify(obj, null, 2);
-  files[file + ".json"] = { contents: new Buffer(json) };
+  serialize(files, file, obj);
   delete files[file];
 }
 
@@ -24,8 +39,12 @@ function processFile(files, file, builder) {
 function improvisor(){
   return function(files, metalsmith, done) {
     Object.keys(files).forEach(function(file) {
-      var builder = (extname(file) == '.ls') ? build.leadsheet :
-        (file.indexOf('My.') > 0) ? build.vocab : null;
+      var builder;
+      if (extname(file) == '.ls') {
+        builder = build.leadsheet;
+      } else if (file.indexOf('My.') > 0) {
+        builder = build.vocab;
+      }
 
       if(builder) {
         processFile(files, file, builder);
