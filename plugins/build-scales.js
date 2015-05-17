2c9a1280e8336c2d7e3a124f1994cee638f6f6bf
danigb <danigb@gmail.com>
2015-05-17 15:16:48 +0200

    Split scales: jazz / esoteric, spell / alias

--- plugins/build-scales.js
+++ plugins/build-scales.js
@@ -4,7 +4,36 @@ module.exports = function(files, metalsmith, done) {
   var teo = buildTeoria(clean);
   files['scales.json'] = serialize(clean);
   files['scales-teoria.json'] = serialize(teo);
+  var scales = splitScales(teo);
+  files['scales-jazz.json'] = serialize(scales.jazz.spell);
+  files['scales-jazz-alias.json'] = serialize(scales.jazz.alias);
+  files['scales-esoteric.json'] = serialize(scales.esoteric.spell);
+  files['scales-esoteric-alias.json'] = serialize(scales.esoteric.alias);
   done();
+}
+
+function splitScales(teoria) {
+  var scales = { jazz: { spell: {}, alias: {}}, esoteric: {spell: {}, alias: {}}};
+  for(var name in teoria) {
+    var group = isJazzName(name) ? scales.jazz : scales.esoteric;
+    if(teoria[name].spell) {
+      group.spell[name] = teoria[name].spell;
+    } else if(teoria[name].same) {
+      group.alias[name] = teoria[name].same;
+    } else {
+      throw Error('Invalid scale: ' + name);
+    }
+  }
+  return scales;
+}
+
+var JAZZ = "major minor dominant dimished bebop ionian dorian lydian mixolydian aeolian phrygian locrian melodic whole".split(' ');
+function isJazzName(name) {
+  if(name == 'altered' || name == 'blues' || name == 'pentatonic' ) return true;
+  for(var jazz of JAZZ) {
+    if(name.indexOf(jazz) >= 0) return true;
+  }
+  return false;
 }
 
 function buildTeoria(source) {
